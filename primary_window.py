4927488171ebff79ff865e2b5ad2174126612856
Surya Tejess <suryatejessk@gmail.com>
2024-06-24 17:07:01 +0530

    Added Lab Components

--- primary_window.py
+++ primary_window.py
@@ -8,30 +8,116 @@ import tabula
 root = tk.Tk()
 root.title("Course Code and Slot Selector")
 
-# file dialog box
-root.theory_dialog = filedialog.askopenfilename(initialdir="/", title="Select the theory slots csv file", filetypes = (("pdf files", "*.pdf"), ("all files", "*.*6")) )
-tabula.read_pdf(root.theory_dialog, pages="all")
-tabula.convert_into(root.theory_dialog, root.theory_dialog[:-3]+"csv", pages="all", output_format="csv")
+# File dialog box for theory slots
+theory_dialog = filedialog.askopenfilename(initialdir="/", title="Select the theory slots PDF file", filetypes=(("PDF files", "*.pdf"), ("all files", "*.*")))
+tabula.read_pdf(theory_dialog, pages="all")
+tabula.convert_into(theory_dialog, theory_dialog[:-3] + "csv", pages="all", output_format="csv")
 
-# Read data from CSV into a DataFrame
-df = pd.read_csv(root.theory_dialog[:-3]+"csv")
-course_data1 = {}
-for index, row in df.iterrows():
+# Read data from theory CSV into a DataFrame
+df_theory = pd.read_csv(theory_dialog[:-3] + "csv")
+course_data_theory = {}
+for index, row in df_theory.iterrows():
     course = row['COURSE CODE']
     slot = row['SLOT']
-    if course in course_data1:
-        course_data1[course].append(slot)
+    if course in course_data_theory:
+        course_data_theory[course].append(slot)
     else:
-        course_data1[course] = [slot]
+        course_data_theory[course] = [slot]
 
-# TODO: read data from Lab_Slots.csv
+# File dialog box for lab slots
+lab_dialog = filedialog.askopenfilename(initialdir="/", title="Select the lab slots PDF file", filetypes=(("PDF files", "*.pdf"), ("all files", "*.*")))
+tabula.read_pdf(lab_dialog, pages="all")
+tabula.convert_into(lab_dialog, lab_dialog[:-3] + "csv", pages="all", output_format="csv")
+
+# Read data from lab CSV into a DataFrame
+df_lab = pd.read_csv(lab_dialog[:-3] + "csv")
+course_data_lab = {}
+for index, row in df_lab.iterrows():
+    course = row['COURSE CODE']
+    slot = row['SLOT']
+    if course in course_data_lab:
+        course_data_lab[course].append(slot)
+    else:
+        course_data_lab[course] = [slot]
 
 selected_slots = {}  # Dictionary to store selected slots and their colors
 
+# Define conflicting slots
+conflict_pairs = [
+    ['TF1', 'L1'],
+    ['TA1', 'L2'],
+    ['E1', 'STC2', 'L3'],
+    ['D1', 'L4'],
+    ['B1', 'L5'],
+    ['L6'],
+    ['TA2', 'L31'],
+    ['E2', 'STC1', 'L32'],
+    ['D2', 'L33'],
+    ['B2', 'L34'],
+    ['TF2', 'L35'],
+    ['L36'],
+    ['TCC1', 'L7'],
+    ['E1', 'STA2', 'L8'],
+    ['G1', 'TFF1', 'L9'],
+    ['TBB1', 'L10'],
+    ['TDD1', 'L11'],
+    ['L12'],
+    ['E2', 'STA1', 'L37'],
+    ['G2', 'TFF2', 'L38'],
+    ['TBB2', 'L39'],
+    ['TDD2', 'L40'],
+    ['TCC2', 'L41'],
+    ['L42'],
+    ['TE1', 'L13'],
+    ['C1', 'L14'],
+    ['A1', 'L15'],
+    ['F1', 'L16'],
+    ['D1', 'L17'],
+    ['L18'],
+    ['C2', 'L43'],
+    ['A2', 'L44'],
+    ['F2', 'L45'],
+    ['D2', 'L46'],
+    ['TE2', 'L47'],
+    ['L48'],
+    ['TAA1', 'L19'],
+    ['TD1', 'L20'],
+    ['B1', 'L21'],
+    ['G1', 'TEE1', 'L22'],
+    ['C1', 'L23'],
+    ['L24'],
+    ['TD2', 'L49'],
+    ['B2', 'L50'],
+    ['G2', 'TEE2', 'L51'],
+    ['C2', 'L52'],
+    ['TAA2', 'L53'],
+    ['L54'],
+    ['TG1', 'L25'],
+    ['TB1', 'L26'],
+    ['TC1', 'L27'],
+    ['A1', 'L28'],
+    ['F1', 'L29'],
+    ['L30'],
+    ['TB2', 'L55'],
+    ['TC2', 'L56'],
+    ['A2', 'L57'],
+    ['F2', 'L58'],
+    ['TG2', 'L59'],
+    ['L60']
+]
+
+conflict_dict = {}
+for pair in conflict_pairs:
+    for slot in pair:
+        if slot not in conflict_dict:
+            conflict_dict[slot] = set()
+        conflict_dict[slot].update(pair)
+        conflict_dict[slot].remove(slot)
+
 # Function to update the slot dropdown based on the selected course code
-def update_slot_dropdown(event, slot_dropdown, course_code_var):
+def update_slot_dropdown(event, slot_dropdown, course_code_var, course_data):
     selected_course = course_code_var.get()
-    slots = course_data1.get(selected_course, ["N/A"])
+    slots = course_data.get(selected_course, ["N/A"])
     slot_dropdown['values'] = slots
 
 # Function to add new set of dropdown menus
@@ -51,29 +137,15 @@ def add_dropdowns():
         color = color_var.get()
         if slot != "N/A" and color != "Choose Color":
             slot_parts = slot.split('+')
-            # Check if any part of the slot is already selected or conflicts with existing selections
-            conflict_pairs = {
-                "E1": ["STC2", "STA2"],
-                "STC2": ["E1"],
-                "E2": ["STC1", "STA1"],
-                "STC1": ["E2"],
-                "STA2": ["E1"],
-                "STA1": ["E2"],
-                "G1": ["TFF1", "TEE1"],
-                "TFF1": ["G1"],
-                "G2": ["TFF2", "TEE2"],
-                "TFF2": ["G2"],
-                "TEE1": ["G1"],
-                "TEE2": ["G2"]
-            }
             for part in slot_parts:
                 if part in selected_slots:
                     messagebox.showwarning("Slot Clash", f"The slot '{part}' is already filled by another course.")
                     return
-                for conflict in conflict_pairs.get(part, []):
-                    if conflict in selected_slots:
-                        messagebox.showwarning("Slot Clash", f"The slot '{part}' cannot be selected when '{conflict}' is already selected.")
-                        return
+                if part in conflict_dict:
+                    for conflict in conflict_dict[part]:
+                        if conflict in selected_slots:
+                            messagebox.showwarning("Slot Clash", f"The slot '{part}' cannot be selected when '{conflict}' is already selected.")
+                            return
             for part in slot_parts:
                 selected_slots[part] = color_dict[color]
         update_table()
@@ -81,20 +153,28 @@ def add_dropdowns():
     course_code_var = tk.StringVar()
     slot_var = tk.StringVar()
     color_var = tk.StringVar()
+    course_type_var = tk.StringVar(value="Theory")
 
     # Create a new frame for each set of dropdowns
-    frame = tk.Frame(root)
-    frame.pack(pady=5)
+    frame = tk.Frame(main_frame)
+    frame.pack(pady=5, anchor='w')
+
+    # Theory/ Lab Radio Buttons
+    theory_radio = ttk.Radiobutton(frame, text="Theory", variable=course_type_var, value="Theory", command=lambda: update_slot_dropdown(None, slot_dropdown, course_code_var, course_data_theory))
+    lab_radio = ttk.Radiobutton(frame, text="Lab", variable=course_type_var, value="Lab", command=lambda: update_slot_dropdown(None, slot_dropdown, course_code_var, course_data_lab))
+
+    theory_radio.pack(side=tk.LEFT, padx=5)
+    lab_radio.pack(side=tk.LEFT, padx=5)
 
     # Course Code Dropdown
     course_code_label = ttk.Label(frame, text="Select Course Code:")
     course_code_label.pack(side=tk.LEFT, padx=5)
 
     course_code_dropdown = ttk.Combobox(frame, textvariable=course_code_var)
-    course_code_dropdown['values'] = list(course_data1.keys())
+    course_code_dropdown['values'] = list(course_data_theory.keys())
     course_code_dropdown.pack(side=tk.LEFT, padx=5)
     course_code_dropdown.bind("<<ComboboxSelected>>",
-                              lambda event: update_slot_dropdown(event, slot_dropdown, course_code_var))
+                              lambda event: update_slot_dropdown(event, slot_dropdown, course_code_var, course_data_theory if course_type_var.get() == "Theory" else course_data_lab))
 
     # Slot Dropdown
     slot_label = ttk.Label(frame, text="Select Slot:")
@@ -120,8 +200,8 @@ def add_dropdowns():
     cancel_button.pack(side=tk.RIGHT, padx=5)
 
     # Set default course code and slot
-    course_code_var.set(list(course_data1.keys())[0])
-    update_slot_dropdown(None, slot_dropdown, course_code_var)
+    course_code_var.set(list(course_data_theory.keys())[0])
+    update_slot_dropdown(None, slot_dropdown, course_code_var, course_data_theory)
 
     # Move the '+' button below the new frame
     plus_button_frame.pack_forget()
@@ -140,8 +220,12 @@ def update_table():
                 else:
                     labels[I][j].config(bg="white")
 
+# Create a frame for the course selection and table
+main_frame = tk.Frame(root)
+main_frame.pack(side=tk.TOP, fill='both', expand=True)
+
 # Create a frame for the '+' button
-plus_button_frame = tk.Frame(root)
+plus_button_frame = tk.Frame(main_frame)
 plus_button_frame.pack(pady=5)
 
 # Create the '+' button
@@ -150,19 +234,17 @@ plus_button.pack()
 
 # Define the table data
 table_data = [
-    ["Day/Time", "8 - 9", "9 - 10", "10 - 11", "11 - 12", "12 - 1", "2 - 3", "3 - 4", "4 - 5", "5 - 6", "6 - 7"],
-    ["Tue", "TF1", "TA1", "E1+STC2", "D1", "B1", "TA2", "E2+STC1", "D2", "B2", "TF2"],
-    ["Wed", "TCC1", "E1+STA2", "G1+TFF1", "TBB1", "TDD1", "E2+STA1", "G2+TFF2", "TBB2", "TDD2", "TCC2"],
-    ["Thu", "TE1", "C1", "A1", "F1", "D1", "C2", "A2", "F2", "D2", "TE2"],
-    ["Fri", "TAA1", "TD1", "B1", "G1+TEE1", "C1", "TD2", "B2", "G2+TEE2", "C2", "TAA2"],
-    ["Sat", "TG1", "TB1", "TC1", "A1", "F1", "TB2", "TC2", "A2", "F2", "TG2"]
+    ["Day/Time",  "8 - 9","9 - 10","10 - 11","11 - 12","12 - 1","1 - 1:30","2 - 3","3 - 4","4 - 5","5 - 6","6 - 7","7 - 7:30"],
+    ["Tue","TF1+L1","TA1+L2","E1+STC2+L3","D1+L4","B1+L5","L6","TA2+L31","E2+STC1+L32","D2+L33","B2+L34","TF2+L35","L36"],
+    ["Wed","TCC1+L7","E1+STA2+L8","G1+TFF1+L9","TBB1+L10","TDD1+L11","L12","E2+STA1+L37","G2+TFF2+L38","TBB2+L39","TDD2+L40","TCC2+L41","L42"],
+    ["Thu","TE1+L13","C1+L14","A1+L15","F1+L16","D1+L17","L18","C2+L43","A2+L44","F2+L45","D2+L46","TE2+L47","L48"],
+    ["Fri","TAA1+L19","TD1+L20","B1+L21","G1+TEE1+L22","C1+L23","L24","TD2+L49","B2+L50","G2+TEE2+L51","C2+L52","TAA2+L53","L54"],
+    ["Sat","TG1+L25","TB1+L26","TC1+L27","A1+L28","F1+L29","L30","TB2+L55","TC2+L56","A2+L57","F2+L58","TG2+L59","L60"]
 ]
 
 # Create a frame for the table
-table_frame = tk.Frame(root)
+table_frame = tk.Frame(main_frame)
 table_frame.pack(pady=10)
-
-# Create a grid of labels and keep a reference to the labels
 labels = []
 for I in range(len(table_data)):  # Rows
     row_labels = []
@@ -172,32 +254,13 @@ for I in range(len(table_data)):  # Rows
         row_labels.append(label)
     labels.append(row_labels)
 
-# Define the colors and their codes
 color_dict = {
-    "Choose Color": "white",
     "Red": "#FF0000",
     "Green": "#00FF00",
     "Blue": "#0000FF",
     "Yellow": "#FFFF00",
     "Purple": "#800080",
     "Orange": "#FFA500",
-    "Pink": "#FFC0CB",
-    "LightBlue": "#ADD8E6",
-    "LightGreen": "#90EE90",
-    "LightYellow": "#FFFFE0",
-    "LightCoral": "#F08080",
-    "LightSalmon": "#FFA07A",
-    "LightGray": "#D3D3D3",
-    "DarkRed": "#8B0000",
-    "DarkGreen": "#006400",
-    "DarkBlue": "#00008B",
-    "DarkYellow": "#FFD700",
-    "DarkPurple": "#4B0082",
-    "DarkOrange": "#FF8C00"
 }
 
-# Start the Tkinter event loop
 root.mainloop()
-
-# TODO: add a submit button to save the selected slots to a CSV file
-# TODO: ics file generation for the whole semester
